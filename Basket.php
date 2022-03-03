28666bac281a5da580ebd8a00352ce231bbb7a1b
Xinja96 <lewishowson@hotmail.co.uk>
2022-03-03 15:31:48 +0000

    Update
    Added price and checkout

--- Basket.php
+++ Basket.php
@@ -8,43 +8,51 @@
     <body style="font-family: Poppins;">
 
     <?php 
-        $quantity = 0;
-
+        $quantity1 = isset($_POST["quantity1"]) && $_POST["quantity1"] > 0 ? $_POST["quantity1"] : 1;
+        $quantity2 = isset($_POST["quantity2"]) && $_POST["quantity2"] > 0 ? $_POST["quantity2"] : 1;
         if($_POST){
             
-            $quantity = isset($_POST["quantity"])? $_POST["quantity"] : 1;
-            
-            if(isset($_POST["dec"])){
-                echo "dec";
-                $quantity--;
-                echo $quantity;
+            if(isset($_POST["dec1"]) && $quantity1 > 1){
+                $quantity1--;
             } 
-            if(isset($_POST["inc"])){
-                echo "inc";
-                $quantity++;
-                echo $quantity;
+            if(isset($_POST["inc1"])){
+                $quantity1++;
+            }
+
+            if(isset($_POST["dec2"]) && $quantity2 > 1){
+                $quantity2--;
+            } 
+            if(isset($_POST["inc2"])){
+                $quantity2++;
             }
             
         }
     ?>
+    <div class="title"><h1>YOUR BASKET</h1></div>
 
     <div class="basket">
-        <div style="font-size: 14px;"><h1>YOUR BASKET</h1></div>
-        <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST">
-            <div class="items">
-                <div class="item">
-                    <img src="Craghopper.png" alt="Craghopper" height="100px" width="100px">
-                    <div class="itemName">Craghoppers Creevey Jacket Waterproof Jacket <span style="font-size: 12px; color:gray; line-height: 15px">Posidon Blue<br> Medium / 40 inch</span></div>
-                    <div class="quantity"><button name="dec">-</button> <input type="number" name="quantity" style="width: 30px; height: 28px; border: 0px;" value="<?php echo $quantity; ?>" readonly> <button name="inc">+</button></div>
-                </div>
-                <hr>
-                <div class="item">
-                    <img src="Volcom.png" alt="Volcom" height="100px" width="100px">
-                    <div class="itemName">Volcom Scortch Insulated Snow Jacket <span style="font-size: 12px; color:gray; line-height: 15px">Acid Black<br>Large / 42 - 44inch</span></div>
-                </div>
-                <hr>
+        <form class="items" action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST">
+            <button class="hidden"></button>
+            <div class="item">
+                <img src="Craghopper.png" alt="Craghopper" height="100px" width="100px"><div class="itemdes">
+                <div class="itemName">Craghoppers Creevey Jacket Waterproof Jacket <span style="font-size: 12px; color:gray; line-height: 15px">Posidon Blue<br> Medium / 40 inch</span></div>
+                <div class="quantity"><button name="dec1">-</button> <input type="number" name="quantity1" style="width: 30px; height: 28px; border: 0px;" value="<?php echo $quantity1; ?>"> <button name="inc1">+</button></div>
+                <div class="price">£59.95</div></div>
             </div>
+            <hr>
+            <div class="item">
+                <img src="Volcom.png" alt="Volcom" height="100px" width="100px"><div class="itemdes">
+                <div class="itemName">Volcom Scortch Insulated Snow Jacket <span style="font-size: 12px; color:gray; line-height: 15px">Acid Black<br>Large / 42 - 44inch</span></div>
+                <div class="quantity"><button name="dec2">-</button> <input type="number" name="quantity2" style="width: 30px; height: 28px; border: 0px;" value="<?php echo $quantity2; ?>"> <button name="inc2">+</button></div>
+                <div class="price">£105.95</div></div>
+            </div>
+            <hr>
         </form>
+        <div class="checkout">
+            <div class="cost"> <span class="subtotal">Sub-total <span>£165.90</span></span> <br><br> <span class="total" >TOTAL COST <span>£165.90</span></span> </div>
+            <div class="promo"><span style="font-weight: bold;">Add Promotional Code</span> <span style="font-size: small;">One code per order</span> <span style="display:flex; margin-top: 5px;"><input class="promobox" type="text" name="promo" placeholder="Promo code"><button class="promobtn">></button></span> </div>
+            <div><button class="proceedbtn">PROCEED TO CHECKOUT</button></div>
+        </div>
     </div>
         
     </body>
